37454d07dabe66bbbfecaeccb7ff130641b56020
Ardee Aram <ardee.aram@galoretv.com>
2015-02-06 04:03:57 +0800

    get all messages + return JSON format

--- Dongle.rb
+++ Dongle.rb
@@ -1,4 +1,5 @@
 require 'gsm_modem'
+require 'json'
 
 class Dongle
 
@@ -14,15 +15,51 @@ class Dongle
 		scrub @gsm_modem.execute "AT+CGMM"
 	end
 	
+	def send_message(number, message)
+		@gsm_modem.execute "AT+CMGF=1"
+		@gsm_modem.execute %Q(AT+CMGS="#{number}"\r\n)
+		scrub @gsm_modem.execute %Q(#{message}\x1a)
+	end
+	
+	def messages
+		messages = @gsm_modem.execute %Q(AT+CMGL="ALL")
+		messages = scrub messages
+				
+		#puts "#{messages.split("\r\n").count} messages"
+		
+		message_array = []
+		message_array_item = {}
+		messages.split("\r\n").each_with_index do |line, index|
+			message_index = index / 2
+			
+			if index % 2 == 0 	# header
+				
+				x, header = line.split("CMGL:")
+				message_index, status, carrier, x, smsdate, smstime = header.split(",")
+				message_array_item = {
+					index: message_index.to_i,
+					status: status.sub(/^"(.*)"$/, '\1'),
+					carrier: carrier.sub(/^"(.*)"$/, '\1'),
+					smsdate: smsdate.sub(/^"(.*)/, '\1'),
+					smstime: smstime.sub(/"(.*)$/, '\1')
+				}
+			else				# message body
+				message_array_item[:message] = line
+				message_array[message_index] = message_array_item
+			end
+		end
+		
+		message_array.to_json
+		#messages
+	end
+	
 	def close
 		@gsm_modem.close if !@gsm_modem.nil?
 	end
 	
 	#cleans the output of GSM geekiness
 	def scrub(text)
-		text = text.gsub(/(\r\nOK\r\n)$/, '')
-		text = text.gsub(/[\r\n]/, '')
-		text = text.chomp
+		text.gsub(/(\r\nOK\r\n)$/, '').strip
 	end
 
 end
--- Rakefile
+++ Rakefile
@@ -2,9 +2,24 @@ task :default do
 	puts "Project Lost Treasure"
 end
 
-task :dev do
-	require 'dongle'
-	dongle = Dongle.new "COM4"
-	puts dongle.manufacturer + " " + dongle.model
-	dongle.close
+namespace :dev do
+	task :dongle do
+		require 'dongle'
+		dongle = Dongle.new "COM4"
+		begin
+		#puts dongle.manufacturer + " " + dongle.model
+		#puts dongle.send_message "222", "BAL"
+			puts dongle.messages
+		ensure
+			dongle.close if !dongle.nil?
+		end
+	end
+	
+	task :gsm_modem do
+		require 'gsm_modem'
+		gsm_modem = Gsm_Modem.new "COM4"
+		gsm_modem.execute %Q(AT+CMGF=1)   
+		puts gsm_modem.execute %Q(AT+CMGL="ALL")
+		gsm_modem.close
+	end
 end
