cefa21d516d7cf4a06efa307411c66e6fa947114
Ardee Aram (Codebox) <ardee.aram@galoretv.com>
2015-03-26 11:51:49 +0800

    Exploration of NodeJS as frontend server

--- frontend-nodejs/lost-treasure.js
+++ frontend-nodejs/lost-treasure.js
@@ -4,6 +4,8 @@
 
 var sys = require("sys"),  
 my_http = require("http");  
+var AWS = require('aws-sdk'); 
+
 my_http.createServer(function(request,response){  
     //sys.puts("I got kicked");  
     response.writeHeader(200, {"Content-Type": "text/plain"});  
